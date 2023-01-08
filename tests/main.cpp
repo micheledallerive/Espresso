674ddc433952f82aa4e74256a0ec45fcce456c60
Michele Dalle Rive <dallem@usi.ch>
2023-01-08 17:20:38 +0100

    Test: Added espresso linking in tests.

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,4 +3,12 @@
 //
 
 #include <gtest/gtest.h>
+#include "../src/HTTPMessage.h"
 
+TEST(HTTPMessage, testParseHeaders) {
+  std::string headers = "Content-Type: text/html\r\n"
+                        "Content-Length: 123\r\n"
+                        "Connection: keep-alive\r\n";
+  Espresso::HTTPMessage message("HTTP/1.1", headers, "");
+  ASSERT_EQ(message.getHeader("Content-Type"), "text/html");
+}
