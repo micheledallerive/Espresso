9fff6fd4054b4918d30ea2fd4b5e64569f768438
Michele Dalle Rive <dallem@usi.ch>
2023-01-09 00:00:24 +0100

    HTTPMethod: Created HTTP method class.

---
+++ src/HTTPMethod.cpp
@@ -0,0 +1,49 @@
+//
+// Created by michele on 08.01.23.
+//
+
+#include "HTTPMethod.h"
+
+namespace Espresso {
+
+std::string methodToString(HTTPMethod method) {
+  switch (method) {
+    case GET:return "GET";
+    case POST:return "POST";
+    case PUT:return "PUT";
+    case DELETE:return "DELETE";
+    case HEAD:return "HEAD";
+    case OPTIONS:return "OPTIONS";
+    case PATCH:return "PATCH";
+    case TRACE:return "TRACE";
+    case CONNECT:return "CONNECT";
+  }
+}
+
+HTTPMethod stringToMethod(std::string method) {
+  // make method uppercase
+  std::transform(method.begin(), method.end(), method.begin(), ::toupper);
+  if (method == "GET") {
+    return GET;
+  } else if (method == "POST") {
+    return POST;
+  } else if (method == "PUT") {
+    return PUT;
+  } else if (method == "DELETE") {
+    return DELETE;
+  } else if (method == "HEAD") {
+    return HEAD;
+  } else if (method == "OPTIONS") {
+    return OPTIONS;
+  } else if (method == "PATCH") {
+    return PATCH;
+  } else if (method == "TRACE") {
+    return TRACE;
+  } else if (method == "CONNECT") {
+    return CONNECT;
+  } else {
+    return GET;
+  }
+}
+
+} // Espresso
---
+++ src/HTTPMethod.h
@@ -0,0 +1,29 @@
+//
+// Created by michele on 08.01.23.
+//
+
+#ifndef ESPRESSO_SRC_HTTPMETHOD_H_
+#define ESPRESSO_SRC_HTTPMETHOD_H_
+
+#include <string>
+namespace Espresso {
+
+enum HTTPMethod {
+  GET,
+  POST,
+  PUT,
+  DELETE,
+  HEAD,
+  OPTIONS,
+  PATCH,
+  TRACE,
+  CONNECT
+};
+
+std::string methodToString(HTTPMethod method);
+
+HTTPMethod stringToMethod(std::string method);
+
+} // Espresso
+
+#endif //ESPRESSO_SRC_HTTPMETHOD_H_
