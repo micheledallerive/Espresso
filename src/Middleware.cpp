7076ced6140da6cd1e41884c18d207fefcbabd43
Michele Dalle Rive <dallem@usi.ch>
2023-01-08 23:06:38 +0100

    Middleware: Added Middleware and MiddlewareList

--- src/Middleware.cpp
+++ src/Middleware.cpp
@@ -5,4 +5,25 @@
 #include "Middleware.h"
 
 namespace Espresso {
+
+MiddlewareList::MiddlewareList() = default;
+
+MiddlewareList::~MiddlewareList() = default;
+
+void MiddlewareList::use(const Middleware &middleware) {
+  this->middlewares_.push_back(middleware);
+}
+
+void MiddlewareList::run(HTTPRequest *request, HTTPResponse *response) {
+  std::function<void()> next = []() {};
+  for (auto it = this->middlewares_.begin(); it != this->middlewares_.end();
+       ++it) {
+    auto current = *it;
+    next = [current, request, response, next]() {
+      current(request, response, next);
+    };
+    next();
+  }
+}
+
 } // Espresso
--- src/Middleware.h
+++ src/Middleware.h
@@ -5,13 +5,27 @@
 #ifndef ESPRESSO_SRC_MIDDLEWARE_H_
 #define ESPRESSO_SRC_MIDDLEWARE_H_
 
+#include <functional>
 #include "HTTPRequest.h"
 #include "HTTPResponse.h"
 
 namespace Espresso {
 
-using Middleware = std::function<void(Request&, Response&)>;
+using Middleware = std::function<void(HTTPRequest *request,
+                                      HTTPResponse *response,
+                                      std::function<void()> next)>;
+
+class MiddlewareList {
+ public:
+  MiddlewareList();
+  virtual ~MiddlewareList();
+  void use(const Middleware& middleware);
+  void run(HTTPRequest *request, HTTPResponse *response);
+ private:
+  std::vector<Middleware> middlewares_;
+};
 
 } // Espresso
+
 
 #endif //ESPRESSO_SRC_MIDDLEWARE_H_
