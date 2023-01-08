582e0eb4361362bc676415bb7ad50c2c726a637a
Michele Dalle Rive <dallem@usi.ch>
2023-01-09 00:26:11 +0100

    HTTPResponse: Added toString method.

--- src/HTTPResponse.cpp
+++ src/HTTPResponse.cpp
@@ -8,6 +8,7 @@ namespace Espresso {
 
 HTTPResponse::HTTPResponse() : HTTPMessage() {
   this->status_ = 200;
+  this->version_ = "HTTP/1.1";
 }
 
 HTTPResponse::~HTTPResponse() = default;
@@ -15,6 +16,15 @@ HTTPResponse::~HTTPResponse() = default;
 HTTPResponse *HTTPResponse::setStatus(int status) {
   this->status_ = status;
   return this;
+}
+
+std::string HTTPResponse::toString() {
+  std::string response =
+      this->version_ + " " + std::to_string(this->status_) + " " + "OK" + "\n";
+  response += this->headersToString();
+  response += "\n";
+  response += this->body_;
+  return response;
 }
 
 } // Espresso
