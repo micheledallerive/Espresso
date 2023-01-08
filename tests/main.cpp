8fa968b9d44c581585a9ab733dd2937fd8beb265
Michele Dalle Rive <dallem@usi.ch>
2023-01-09 00:06:45 +0100

    Test: Added middleware tests.

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,6 +5,8 @@
 #include <gtest/gtest.h>
 #include "../src/HTTPMessage.h"
 #include "../src/HTTPRequest.h"
+#include "../src/HTTPResponse.h"
+#include "../src/Middleware.h"
 
 TEST(HTTPMessage, testParseHeaders) {
   std::string headers = "Content-Type: text/html\r\n"
@@ -39,18 +41,73 @@ TEST(HTTPRequest, testConstructor) {
 }
 
 TEST(HTTPRequest, testConstructorFromRequest) {
-    std::string request = "GET /index.html HTTP/1.1\r\n"
-                            "Content-Type: text/html\r\n"
-                            "Content-Length: 123\r\n"
-                            "Connection: keep-alive\r\n"
-                            "\r\n"
-                            "Hello World!";
-    Espresso::HTTPRequest req(request);
-    ASSERT_EQ(req.getMethod(), "GET");
-    ASSERT_EQ(req.getPath(), "/index.html");
-    ASSERT_EQ(req.getVersion(), "HTTP/1.1");
-    ASSERT_EQ(req.getHeader("Content-Type"), "text/html");
-    ASSERT_EQ(req.getHeader("Content-Length"), "123");
-    ASSERT_EQ(req.getHeader("Connection"), "keep-alive");
-    ASSERT_EQ(req.getBody(), "Hello World!");
+  std::string request = "GET /index.html HTTP/1.1\r\n"
+                        "Content-Type: text/html\r\n"
+                        "Content-Length: 123\r\n"
+                        "Connection: keep-alive\r\n"
+                        "\r\n"
+                        "Hello World!";
+  Espresso::HTTPRequest req(request);
+  ASSERT_EQ(req.getMethod(), "GET");
+  ASSERT_EQ(req.getPath(), "/index.html");
+  ASSERT_EQ(req.getVersion(), "HTTP/1.1");
+  ASSERT_EQ(req.getHeader("Content-Type"), "text/html");
+  ASSERT_EQ(req.getHeader("Content-Length"), "123");
+  ASSERT_EQ(req.getHeader("Connection"), "keep-alive");
+  ASSERT_EQ(req.getBody(), "Hello World!");
+}
+
+TEST(Middlewares, testMiddlewareExecute) {
+  auto *list = new Espresso::MiddlewareList();
+  list->use([](Espresso::HTTPRequest *req,
+               Espresso::HTTPResponse *res,
+               auto next) {
+    res->setHeader("Content-Type", "text/html");
+    res->setHeader("Content-Length", "000");
+  });
+  auto *req = new Espresso::HTTPRequest("GET", "/", "HTTP/1.1", "", "");
+  auto *res = new Espresso::HTTPResponse();
+  list->run(req, res);
+  ASSERT_TRUE(res->getHeader("Content-Type") == "text/html");
+}
+
+TEST(Middlewares, testMiddlewaresContinue) {
+  auto *list = new Espresso::MiddlewareList();
+  list->use([](Espresso::HTTPRequest *req,
+               Espresso::HTTPResponse *res,
+               auto next) {
+    res->setHeader("Content-Type", "text/html");
+    res->setHeader("Content-Length", "000");
+    next();
+  });
+  list->use([](Espresso::HTTPRequest *req,
+               Espresso::HTTPResponse *res,
+               auto next) {
+    res->setHeader("Content-Length", "123");
+  });
+  auto *req = new Espresso::HTTPRequest("GET", "/", "HTTP/1.1", "", "");
+  auto *res = new Espresso::HTTPResponse();
+  list->run(req, res);
+  ASSERT_EQ(res->getHeader("Content-Type"), "text/html");
+  ASSERT_EQ(res->getHeader("Content-Length"), "123");
+}
+
+TEST(Middlewares, testMiddlewaresBlocked) {
+  auto *list = new Espresso::MiddlewareList();
+  list->use([](Espresso::HTTPRequest *req,
+               Espresso::HTTPResponse *res,
+               auto next) {
+    res->setHeader("Content-Type", "text/html");
+    res->setHeader("Content-Length", "000");
+  });
+  list->use([](Espresso::HTTPRequest *req,
+               Espresso::HTTPResponse *res,
+               auto next) {
+    res->setHeader("Content-Length", "123");
+  });
+  auto *req = new Espresso::HTTPRequest("GET", "/", "HTTP/1.1", "", "");
+  auto *res = new Espresso::HTTPResponse();
+  list->run(req, res);
+  ASSERT_EQ(res->getHeader("Content-Type"), "text/html");
+  ASSERT_EQ(res->getHeader("Content-Length"), "000");
 }
