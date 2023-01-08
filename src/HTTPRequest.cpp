2a7246741ecb41235f6315e52f8f4e4e3fed5d7f
Michele Dalle Rive <dallem@usi.ch>
2023-01-08 18:20:55 +0100

    HTTPRequest: Added constructor from string.

--- src/HTTPRequest.cpp
+++ src/HTTPRequest.cpp
@@ -20,21 +20,28 @@ HTTPRequest::HTTPRequest(std::string method,
 
 HTTPRequest::HTTPRequest(const std::string &request) {
   std::istringstream iss(request);
+  std::string method, path, version;
   std::string line;
   std::getline(iss, line);
-  std::string method = line.substr(0, line.find(' '));
-  std::string path = line.substr(line.find(' ') + 1,
-                                 line.find(' ', line.find(' ') + 1)
-                                     - line.find(' ') - 1);
-  std::string version = line.substr(line.find(' ', line.find(' ') + 1) + 1);
-  version.pop_back(); // remove the \r
+  std::istringstream iss2(line);
+  iss2 >> method >> path >> version;
+
+  std::string headers;
+  while (std::getline(iss, line)) {
+    if (line == "\r") {
+      break;
+    }
+    headers += line + "\n";
+  }
+  std::string body;
+  while (std::getline(iss, line)) {
+    body += line;
+  }
   this->method_ = std::move(method);
   this->path_ = std::move(path);
   this->version_ = std::move(version);
-  std::getline(iss, line);
-  this->parseHeaders_(line);
-  std::getline(iss, line);
-  this->body_ = std::move(line);
+  this->parseHeaders_(headers);
+  this->body_ = std::move(body);
 }
 
 HTTPRequest::~HTTPRequest() = default;
