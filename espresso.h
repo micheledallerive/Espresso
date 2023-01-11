473819acabd3f25fb5c7511955988e3db973a5c5
Michele Dalle Rive <dallem@usi.ch>
2023-01-11 14:16:06 +0100

    Created header file containing include of all the libraries.

---
+++ espresso.h
@@ -0,0 +1,16 @@
+//
+// Created by michele on 11.01.23.
+//
+
+#ifndef ESPRESSO__ESPRESSO_H_
+#define ESPRESSO__ESPRESSO_H_
+
+#include "src/Server.h"
+#include "src/HTTPResponse.h"
+#include "src/HTTPMessage.h"
+#include "src/HTTPRequest.h"
+#include "src/Cookies.h"
+#include "src/Middleware.h"
+#include "src/Router.h"
+
+#endif //ESPRESSO__ESPRESSO_H_
