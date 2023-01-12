963e2bbcc875483983b448b5bd4d61b297a04b38
Michele Dalle Rive <dallem@usi.ch>
2023-01-12 22:59:10 +0100

    Updated project structure.

---
+++ src/espresso.h
@@ -0,0 +1,16 @@
+//
+// Created by michele on 11.01.23.
+//
+
+#ifndef ESPRESSO__ESPRESSO_H_
+#define ESPRESSO__ESPRESSO_H_
+
+#include "Server.h"
+#include "HTTPResponse.h"
+#include "HTTPMessage.h"
+#include "HTTPRequest.h"
+#include "Cookies.h"
+#include "Middleware.h"
+#include "Router.h"
+
+#endif //ESPRESSO__ESPRESSO_H_
