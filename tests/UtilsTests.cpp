7922e357f405e4d9e26f49ad931ab6289d0670c1
Michele Dalle Rive <dallem@usi.ch>
2023-01-13 00:48:09 +0100

    Test: Added tests for utils.

--- tests/UtilsTests.cpp
+++ tests/UtilsTests.cpp
@@ -1,3 +1,84 @@
 //
 // Created by michele on 10.01.23.
 //
+
+#include <gtest/gtest.h>
+#include "utils.cpp"
+
+namespace Espresso {
+
+TEST(Utils, Split) {
+  std::string test_string = "Hello World";
+  std::vector<std::string> split_string = split(test_string, ' ');
+  ASSERT_EQ(split_string.size(), 2);
+  ASSERT_EQ(split_string[0], "Hello");
+  ASSERT_EQ(split_string[1], "World");
+
+  test_string = "Hello,World,Test,";
+  std::vector<std::string> split_string2 = split(test_string, ',');
+  ASSERT_EQ(split_string2.size(), 3);
+  ASSERT_EQ(split_string2[0], "Hello");
+  ASSERT_EQ(split_string2[1], "World");
+  ASSERT_EQ(split_string2[2], "Test");
+
+}
+
+TEST(Utils, UrlsMatch) {
+  ASSERT_TRUE(urls_match("/api/*", "/api/test"));
+  ASSERT_TRUE(urls_match("/api/*", "/api/test/"));
+  ASSERT_TRUE(urls_match("/api/*", "/api/test/1"));
+  ASSERT_TRUE(urls_match("/api/*", "/api"));
+  ASSERT_TRUE(urls_match("/api/*", "/api/"));
+
+  ASSERT_TRUE(urls_match("/api/:id", "/api/5"));
+  ASSERT_TRUE(urls_match("/api/:id", "/api/5/"));
+  ASSERT_FALSE(urls_match("/api/:id", "/api/5/1"));
+
+  ASSERT_FALSE(urls_match("/api/:id", "/api/5", false));
+  ASSERT_FALSE(urls_match("/api/:id", "/api/5/", false));
+  ASSERT_TRUE(urls_match("/api/:id", "/api/:id", false));
+
+  ASSERT_TRUE(urls_match("/api/test", "/api/test"));
+}
+
+TEST(Utils, GetUTCDate) {
+  std::string d = getUTCDate();
+  ASSERT_EQ(d.length(), 29);
+
+  std::vector<std::string> parts = split(d, ' ');
+  ASSERT_EQ(parts.size(), 6);
+
+  ASSERT_EQ(parts[0].back(), ',');
+  ASSERT_TRUE(isupper(parts[0][0]));
+  ASSERT_TRUE(islower(parts[0][1]));
+  ASSERT_TRUE(islower(parts[0][2]));
+
+  ASSERT_EQ(parts[1].length(), 2);
+
+  ASSERT_EQ(parts[2].length(), 3);
+  ASSERT_TRUE(isupper(parts[2][0]));
+  ASSERT_TRUE(islower(parts[2][1]));
+  ASSERT_TRUE(islower(parts[2][2]));
+
+  ASSERT_EQ(parts[3].length(), 4);
+
+  ASSERT_EQ(parts[4].length(), 8);
+
+  ASSERT_EQ(parts[5], "GMT");
+}
+
+TEST(Utils, GetAbsolutePath) {
+  Settings settings = {
+      {"BASE_PATH", std::string("/test/")}
+  };
+  Server server(settings);
+  ASSERT_EQ(getAbsolutePath("/test"), "/test");
+  ASSERT_EQ(getAbsolutePath("/test/"), "/test/");
+  ASSERT_EQ(getAbsolutePath("/test/test"), "/test/test");
+
+  ASSERT_EQ(getAbsolutePath("hello"), "/test/hello");
+  ASSERT_EQ(getAbsolutePath("hello/"), "/test/hello/");
+  ASSERT_EQ(getAbsolutePath("hello/test"), "/test/hello/test");
+}
+
+}
