1a814ed257aa072202916e7f42a3b3a4b9f797e1
Michele Dalle Rive <dallem@usi.ch>
2023-01-13 00:31:01 +0100

    Test: Added some tests for Server (it's hard to test something that loops forever lol)

--- tests/ServerTests.cpp
+++ tests/ServerTests.cpp
@@ -1,3 +1,29 @@
 //
 // Created by michele on 10.01.23.
 //
+
+#include <gtest/gtest.h>
+#include "Server.cpp"
+
+namespace Espresso {
+
+TEST(Server, Constructor) {
+  Server server;
+  EXPECT_EQ(std::any_cast<std::string>(server_settings["BASE_PATH"]),
+            ESPRESSO_BASE_PATH);
+}
+
+TEST(Server, SettingsConstructor) {
+  Settings settings = {
+      {"BASE_PATH", std::string("/path")},
+      {"TEST", std::string("test")}
+  };
+  Server server(settings);
+  ASSERT_EQ(server_settings.size(), 2);
+  EXPECT_EQ(std::any_cast<std::string>(server_settings["BASE_PATH"]),
+            "/path");
+  EXPECT_EQ(std::any_cast<std::string>(server_settings["TEST"]),
+            "test");
+}
+
+}
