11db01fb607e9b942ab42545cb8647459977c29a
Michele Dalle Rive <dallem@usi.ch>
2023-01-13 00:01:32 +0100

    Test: Added testing for middlewares.

--- tests/MiddlewareTests.cpp
+++ tests/MiddlewareTests.cpp
@@ -1,3 +1,110 @@
 //
 // Created by michele on 10.01.23.
 //
+#include <gtest/gtest.h>
+#include "Middleware.h"
+
+namespace Espresso {
+TEST(MiddlewareList, SimpleUse) {
+  MiddlewareList list;
+  list.use([](HTTPRequest *request,
+              HTTPResponse *response,
+              const std::function<void()> &next) {
+    response->setBody("Hello World!");
+    next();
+  });
+  HTTPRequest request(GET, "/", "HTTP/1.1", "", "");
+  HTTPResponse response;
+  ASSERT_EQ(response.getBody(), "");
+  list.run(&request, &response);
+  ASSERT_EQ(response.getBody(), "Hello World!");
+}
+
+TEST(MiddlewareList, ManyMiddlewares) {
+  MiddlewareList list;
+  list.use([](HTTPRequest *request,
+              HTTPResponse *response,
+              const std::function<void()> &next) {
+    response->setBody("Hello World!");
+    next();
+  });
+  list.use([](HTTPRequest *request,
+              HTTPResponse *response,
+              const std::function<void()> &next) {
+    response->setBody(response->getBody() + " Hello World!");
+    next();
+  });
+  HTTPRequest request(GET, "/", "HTTP/1.1", "", "");
+  HTTPResponse response;
+  ASSERT_EQ(response.getBody(), "");
+  list.run(&request, &response);
+  ASSERT_EQ(response.getBody(), "Hello World! Hello World!");
+}
+
+TEST(MiddlewareList, DontNextMiddleware) {
+  MiddlewareList list;
+  list.use([](HTTPRequest *request,
+              HTTPResponse *response,
+              const std::function<void()> &next) {
+    response->setBody("Hello World!");
+  });
+  list.use([](HTTPRequest *request,
+              HTTPResponse *response,
+              const std::function<void()> &next) {
+    response->setBody(response->getBody() + " Hello World!");
+  });
+  HTTPRequest request(GET, "/", "HTTP/1.1", "", "");
+  HTTPResponse response;
+  ASSERT_EQ(response.getBody(), "");
+  list.run(&request, &response);
+  ASSERT_EQ(response.getBody(), "Hello World!");
+}
+
+TEST(MiddlewareList, UseWithPath) {
+  MiddlewareList list;
+  list.use("/hello", [](HTTPRequest *request,
+                        HTTPResponse *response,
+                        const std::function<void()> &next) {
+    response->setBody("Hello!");
+    next();
+  });
+  list.use("/world", [](HTTPRequest *request,
+                        HTTPResponse *response,
+                        const std::function<void()> &next) {
+    response->setBody("World!");
+  });
+  HTTPRequest request(GET, "/hello", "HTTP/1.1", "", "");
+  HTTPResponse response;
+  ASSERT_EQ(response.getBody(), "");
+  list.run(&request, &response);
+  ASSERT_EQ(response.getBody(), "Hello!");
+}
+
+TEST(MiddlewareList, UseWithPathAndNext) {
+  MiddlewareList list;
+  list.use("/hello", [](HTTPRequest *request,
+                        HTTPResponse *response,
+                        const std::function<void()> &next) {
+    response->setBody("Hello!");
+    next();
+  });
+  list.use("/hello", [](HTTPRequest *request,
+                        HTTPResponse *response,
+                        const std::function<void()> &next) {
+    response->setBody(response->getBody() + " World!");
+    next();
+  });
+  list.use("/hell", [](HTTPRequest *request,
+                        HTTPResponse *response,
+                        const std::function<void()> &next) {
+    response->setBody(response->getBody() + " :(");
+  });
+  HTTPRequest request(GET, "/hello", "HTTP/1.1", "", "");
+  HTTPResponse response;
+  ASSERT_EQ(response.getBody(), "");
+  list.run(&request, &response);
+  ASSERT_EQ(response.getBody(), "Hello! World!");
+}
+
+
+}
