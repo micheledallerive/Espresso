e797461edd2365fd92d677431b0214c978768b25
Michele Dalle Rive <dallem@usi.ch>
2023-01-18 00:43:07 +0100

    Test: Added tests and readded test files to CMakeLists.

--- tests/PathRegexTests.cpp
+++ tests/PathRegexTests.cpp
@@ -9,47 +9,16 @@ using namespace Espresso;
 
 TEST(PathRegex, PathToRegex) {
   EXPECT_EQ(PathRegex::pathToRegex("/test"), "/test");
-  EXPECT_EQ(PathRegex::pathToRegex("/test/:id"), "/test/:id");
+  EXPECT_EQ(PathRegex::pathToRegex("/test/:id"), "/test/([^/]+)");
 
   EXPECT_EQ(PathRegex::pathToRegex("/*"), "/.*");
   EXPECT_EQ(PathRegex::pathToRegex("/test/*"), "/test/.*");
-  EXPECT_EQ(PathRegex::pathToRegex("/test/:id/*"), "/test/:id/.*");
-  EXPECT_EQ(PathRegex::pathToRegex("/test/:id/*/:id"), "/test/:id/.*/:id");
+  EXPECT_EQ(PathRegex::pathToRegex("/test/:id/*"), "/test/([^/]+)/.*");
+  EXPECT_EQ(PathRegex::pathToRegex("/test/:id/*/:id"),
+            "/test/([^/]+)/.*/([^/]+)");
 
   EXPECT_EQ(PathRegex::pathToRegex("/test/ab?c"), "/test/ab.?c");
   EXPECT_EQ(PathRegex::pathToRegex("/test/ab+c"), "/test/ab.+c");
-}
-
-TEST(PathRegex, UrlParthMatch) {
-  EXPECT_TRUE(PathRegex::urlPartMatch("test", "test"));
-  EXPECT_FALSE(PathRegex::urlPartMatch("test", "test2"));
-
-  EXPECT_TRUE(PathRegex::urlPartMatch(":id", "test"));
-  EXPECT_TRUE(PathRegex::urlPartMatch(":id", "test2"));
-
-  EXPECT_TRUE(PathRegex::urlPartMatch("te.?st", "test"));
-  EXPECT_TRUE(PathRegex::urlPartMatch("te.?st", "texst"));
-  EXPECT_FALSE(PathRegex::urlPartMatch("te.?st", "test2"));
-  EXPECT_FALSE(PathRegex::urlPartMatch("te.?st", "texxst"));
-
-  EXPECT_FALSE(PathRegex::urlPartMatch("te.+st", "test"));
-  EXPECT_TRUE(PathRegex::urlPartMatch("te.+st", "teest"));
-  EXPECT_TRUE(PathRegex::urlPartMatch("te.+st", "teeeest"));
-  EXPECT_FALSE(PathRegex::urlPartMatch("te.+st", "test2"));
-
-  EXPECT_TRUE(PathRegex::urlPartMatch("te.*st", "test"));
-  EXPECT_TRUE(PathRegex::urlPartMatch("te.*st", "teest"));
-  EXPECT_TRUE(PathRegex::urlPartMatch("te.*st", "teeeest"));
-  EXPECT_FALSE(PathRegex::urlPartMatch("te.*st", "test2"));
-
-  EXPECT_TRUE(PathRegex::urlPartMatch("test.*", "test"));
-  EXPECT_TRUE(PathRegex::urlPartMatch("test.*", "test2"));
-  EXPECT_TRUE(PathRegex::urlPartMatch("test.*", "test2test"));
-  EXPECT_TRUE(PathRegex::urlPartMatch("test.*", "test2test2"));
-
-  EXPECT_TRUE(PathRegex::urlPartMatch(".*test", "test"));
-  EXPECT_TRUE(PathRegex::urlPartMatch(".*test", "2test"));
-  EXPECT_FALSE(PathRegex::urlPartMatch(".*test", "2test2"));
 }
 
 TEST(PathRegex, UrlsMatch) {
