21b8630e6cec162fb8c2bfc399ea0f74d0554703
Michele Dalle Rive <dallem@usi.ch>
2023-01-19 23:39:11 +0100

    ORM: Created enum with SQL types.

---
+++ src/orm/sql/SQLTypes.h
@@ -0,0 +1,19 @@
+//
+// Created by michele on 19.01.23.
+//
+
+#ifndef ESPRESSO_SRC_ORM_SQL_SQLTYPES_H_
+#define ESPRESSO_SRC_ORM_SQL_SQLTYPES_H_
+
+namespace Espresso {
+
+enum class SQLType {
+  INTEGER,
+  REAL,
+  TEXT,
+  BLOB
+};
+
+}
+
+#endif //ESPRESSO_SRC_ORM_SQL_SQLTYPES_H_
