99e128bf533d4d9fdc48c66a24a8565cec23e3c6
Michele Dalle Rive <dallem@usi.ch>
2023-02-08 12:35:48 +0100

    SQLType: Added column info struct.

--- src/orm/sql/SQLTypes.h
+++ src/orm/sql/SQLTypes.h
@@ -14,6 +14,58 @@ enum class SQLType {
   BLOB
 };
 
+SQLType getSQLType(const std::string &type) {
+  if (type == typeid(int).name()) {
+    return SQLType::INTEGER;
+  } else if (type == typeid(float).name()) {
+    return SQLType::REAL;
+  } else if (type == typeid(double).name()) {
+    return SQLType::REAL;
+  } else if (type == typeid(std::string).name()) {
+    return SQLType::TEXT;
+  } else if (type == typeid(bool).name()) {
+    return SQLType::INTEGER;
+  } else {
+    return SQLType::BLOB;
+  }
+}
+
+struct SQLColumnInfo {
+  std::string name;
+  SQLType type{SQLType::BLOB};
+  bool notNull{false};
+  bool primaryKey{false};
+  std::string defaultValue;
+
+  bool operator==(const SQLColumnInfo &other) const {
+    return name == other.name && type == other.type && notNull == other.notNull
+        && primaryKey == other.primaryKey && defaultValue == other.defaultValue;
+  }
+
+  bool operator!=(const SQLColumnInfo &other) const {
+    return !(*this == other);
+  }
+
+  explicit SQLColumnInfo(const std::unordered_map<std::string,
+                                                  std::string> &data) {
+    name = data.at("name");
+    if (data.at("type") == "INTEGER") {
+      type = SQLType::INTEGER;
+    } else if (data.at("type") == "REAL") {
+      type = SQLType::REAL;
+    } else if (data.at("type") == "TEXT") {
+      type = SQLType::TEXT;
+    } else if (data.at("type") == "BLOB") {
+      type = SQLType::BLOB;
+    }
+    notNull = data.at("notnull") == "1";
+    primaryKey = data.at("pk") == "1";
+    defaultValue = data.at("dflt_value");
+  }
+
+  SQLColumnInfo() = default;
+};
+
 }
 
 #endif //ESPRESSO_SRC_ORM_SQL_SQLTYPES_H_
