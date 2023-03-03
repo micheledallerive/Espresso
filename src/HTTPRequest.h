d8d36a1439e18f71441df585b4a31b8c5137dcf9
Michele Dalle Rive <dallem@usi.ch>
2023-03-03 16:30:32 +0100

    HTTPRequest: added body map to hold form data.

--- src/HTTPRequest.h
+++ src/HTTPRequest.h
@@ -11,10 +11,14 @@
 #include "HTTPMethod.h"
 namespace Espresso {
 
-class HTTPRequest : public HTTPMessage{
+class HTTPRequest : public HTTPMessage {
  public:
-  HTTPRequest(HTTPMethod method, std::string path, std::string version, const std::string& headers, std::string body);
-  explicit HTTPRequest(const std::string& request);
+  HTTPRequest(HTTPMethod method,
+              std::string path,
+              std::string version,
+              const std::string &headers,
+              std::string body);
+  explicit HTTPRequest(const std::string &request);
   ~HTTPRequest() override;
 
   HTTPMethod getMethod();
@@ -22,12 +26,13 @@ class HTTPRequest : public HTTPMessage{
   std::unordered_map<std::string, std::string> params;
   std::unordered_map<std::string, std::string> query;
   std::unordered_map<std::string, std::string> cookies;
-  std::unordered_map<std::string, std::any> data;
+  std::unordered_map<std::string, std::any> data; // custom data
+  std::unordered_map<std::string, std::string> body; // the parsed body
  private:
   HTTPMethod method_;
   std::string path_;
-  void parseQuery_(const std::string& queryString);
-  void parseCookies_(const std::string& cookiesString);
+  void parseQuery_(const std::string &queryString);
+  void parseCookies_(const std::string &cookiesString);
 };
 
 } // Espresso
