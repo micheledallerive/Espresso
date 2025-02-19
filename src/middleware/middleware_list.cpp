a545a40678855ffd108ea0bbab99572c381fba96
Michele Dalle Rive <dallerivemichele@gmail.com>
2025-02-19 08:48:19 +0100

    Enforce that each middleware is called precisely once (?)

--- src/middleware/middleware_list.cpp
+++ src/middleware/middleware_list.cpp
@@ -4,7 +4,12 @@ namespace espresso {
 
 http::Response MiddlewareList::run_middlewares(http::Request& request, const std::function<http::Response(http::Request&)>& router)
 {
+    size_t next_idx = 0;
     auto call_chain = [&](auto&& self, size_t idx, http::Request& req) -> http::Response {
+        if (idx != next_idx) {
+            throw std::runtime_error("Each middleware must be called exactly once");
+        }
+        ++next_idx;
         if (idx < m_middlewares.size())
             return m_middlewares[idx](req, [&](http::Request& r) { return self(self, idx + 1, r); });
         return router(req);
